565d7e9248744ee9c568efbfaf64d7f55388f355
rashmika-shiny <shinywork006@gmail.com>
2025-08-10 15:37:25 +0530

    Create booksdb.js

---
+++ booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+  "12345": {
+    title: "Book One",
+    author: "Author A",
+    reviews: { "user1": "Nice book!" }
+  },
+  "67890": {
+    title: "Book Two",
+    author: "Author B",
+    reviews: {}
+  }
+};
+
+module.exports = books;
